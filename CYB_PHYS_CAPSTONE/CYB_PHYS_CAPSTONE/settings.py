8317b8ab4d94a0b9c88f668d8ba9340c22378edd
Sean Scott <sjscott9@asu.edu>
2017-02-08 16:06:20 -0700

    Updated model names, changed port

--- CYB_PHYS_CAPSTONE/CYB_PHYS_CAPSTONE/settings.py
+++ CYB_PHYS_CAPSTONE/CYB_PHYS_CAPSTONE/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'USER': 'admin',
         'PASSWORD': 'abc123',
         'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'PORT': '3306',
     }
 }
 
--- CYB_PHYS_CAPSTONE/home/admin.py
+++ CYB_PHYS_CAPSTONE/home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(NodeController)
+admin.site.register(Battery)
+admin.site.register(Generator)
+admin.site.register(Inverter)
