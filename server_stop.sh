dacbbbf2e437936dd953b2cdfd1d20533679c724
Sean Scott <sjscott9@asu.edu>
2017-02-21 14:31:54 -0700

    Added scripts to start/stop Django and Celery processes

---
+++ server_stop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Stops worker/beat services for Celery and Django server
+# Requires tmux (`sudo apt-get install tmux -y`)
+# Problems? try `sudo chmod a+x server_stop.sh`
+
+tmux kill-session -t celery_beat
+tmux kill-session -t celery_worker
+tmux kill-session -t django
