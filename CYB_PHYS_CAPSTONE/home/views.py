875a316dc74cbe8b40cc2da92ba5334b58c0b71f
alajom <alajom@asu.edu>
2017-02-26 19:27:35 -0700

    Got it to pull data for ONLY ONE NREL OBJECT. Work in progress.
    Have some addditional stuff that I will delete later.

--- CYB_PHYS_CAPSTONE/home/views.py
+++ CYB_PHYS_CAPSTONE/home/views.py
@@ -3,5 +3,10 @@ from home.forms import *
 
 # Create your views here.
 def index(request):
-    form = NREL_Times();
+    form = NREL_Times(initial=NREL.objects.values()[0]);
     return render(request, 'client/NREL.html', {'form':form})
+
+def nrel (request):
+    comp = NREL.objects.filter(wind_dir=171.0).values()[0]
+    form = NREL_Form(initial=comp)
+    return render(request, 'client/NRELview.html', {'form': form})
