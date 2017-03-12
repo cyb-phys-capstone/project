3fbdf6e1ce6659f4ad62ea7b3ddd7ea8a1681299
alajom <alajom@asu.edu>
2017-03-12 14:53:11 -0700

    Got the form to change values

--- CYB_PHYS_CAPSTONE/home/static/NREL_viewer.js
+++ CYB_PHYS_CAPSTONE/home/static/NREL_viewer.js
@@ -2,5 +2,11 @@
 function selectTime () {
     console.log($('#id_timeStamps').val());
     console.log('something');
+
+    var reqObject = {timestamp : $('#id_timeStamps').val()};
+
+    $.get('/NREL_view', reqObject, function(response){
+        $("#NREL_data_viewer").replaceWith(response);
+    })
 }
 
--- CYB_PHYS_CAPSTONE/home/views.py
+++ CYB_PHYS_CAPSTONE/home/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from home.forms import *
 
+
 # Create your views here.
 def index(request):
-    form = NREL_Times(initial=NREL.objects.values()[0]);
-    return render(request, 'client/NREL.html', {'form':form})
+    form = NREL_Times()
+    return render(request, 'client/NREL.html', {'form': form})
 
-def nrel (request):
-    comp = NREL.objects.filter(wind_dir=171.0).values()[0]
+
+def nrel(request):
+    try:
+        comp = NREL.objects.filter(timestamp=request.GET['timestamp']).values()[0]
+    except:
+        comp = NREL.objects.values()[0]
     form = NREL_Form(initial=comp)
     return render(request, 'client/NRELview.html', {'form': form})
+
 
 def DeviceData (request):
     return render(request, 'client/DeviceData.html')
