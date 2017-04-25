7ea09ad8d75c013c4b9f1a1e992616ebd0534bb0
Sean Scott <sjscott9@asu.edu>
2017-04-25 11:51:46 -0700

    Scripts ready for demo

--- server_start.sh
+++ server_start.sh
@@ -9,3 +9,4 @@ tmux new-session -d -s django 'python manage.py runserver'
 tmux new-session -d -s celery_worker 'celery -A CYB_PHYS_CAPSTONE worker -l info'
 tmux new-session -d -s celery_beat 'celery -A CYB_PHYS_CAPSTONE beat'
 cd ..
+tmux new-session -d -s outback 'python outbackSerial.py'
--- server_stop.sh
+++ server_stop.sh
@@ -4,6 +4,7 @@
 # Requires tmux (`sudo apt-get install tmux -y`)
 # Problems? try `sudo chmod a+x server_stop.sh`
 
+tmux kill-session -t outback
 tmux kill-session -t celery_beat
 tmux kill-session -t celery_worker
 tmux kill-session -t django
